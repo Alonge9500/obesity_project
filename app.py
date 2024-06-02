d0f5007824327f3ecb8ad92b0f59d700e7a1a136
alonge9500 <alongedaniel19@gmail.com>
2024-06-02 22:25:06 +0100

    start the streamlit app

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-import streamlit as st 
+import streamlit as st
 
-st.snow()
+st.title('Obesity Check')
+st.header('Fill in the following details', divider='rainbow')
+
+with st.form('obesity_form',clear_on_submit=True):
+    age = st.number_input('Input Age')
+    gender = st.radio('Whats your gender',['Female','Male'])
+    gender_dictionary = {'Female':0,'Male':1}
+    gender = gender_dictionary[gender]
+
+    submit = st.form_submit_button('Submit')
+
+    if submit:
+        st.write("Successful")
+        st.write(gender)
+        st.write(gender_dictionary.items())
